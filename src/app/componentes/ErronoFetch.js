22d33113116a1a5602ad0544334368b388c7e2a9
MonikySouzaLopes <monikysouzalopes@gmail.com>
2024-07-20 16:57:20 -0400

    Criação do componente ErronoFetch - Requisito #5

---
+++ src/app/componentes/ErronoFetch.js
@@ -0,0 +1,7 @@
+'use client'
+//função que será renderizada quando o servidor cair
+export default function ErronoFetch(){
+    return(
+      <center><h1>Vishh, o servidor travou :|</h1></center>  
+    );
+}
