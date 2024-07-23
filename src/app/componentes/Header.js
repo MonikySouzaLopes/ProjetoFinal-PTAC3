1de9dd702d3d646c79775ded6c25495640ad23a6
MonikySouzaLopes <monikysouzalopes@gmail.com>
2024-07-22 20:02:23 -0400

    Estilização da página de produto - #Estilização, coloquei o link para voltar para a página home na logo

--- src/app/componentes/Header.js
+++ src/app/componentes/Header.js
@@ -1,12 +1,21 @@
 import styles from "./header.module.css"
 import Image from "next/image";
+import Link from "next/link"
 export default function Header(){
     return(
         <nav className={styles.navbar}>
         <div className={styles.navbar_div}> 
-        <Image width= {200}
-              height={200} 
-              src="/Logo.png"/>
+
+        <Link href="/">
+                   
+                        <Image
+                            width={200}
+                            height={200}
+                            src="/Logo.png"
+                            alt="Logo"
+                        />
+                   
+                </Link>
         <p>Urban Believers</p>
         <p className={styles.frase}>O lugar onde a fé e a moda se encontram !</p>
         </div>
--- src/app/produto/[id]/page.js
+++ src/app/produto/[id]/page.js
@@ -1,24 +1,28 @@
 
 import Image from "next/image";
-
+import styles from "./produto.module.css";
 export default async function Produto({params}){
     
     const response = await fetch("http://localhost:3000/api/" + params.id);
    
     const data = await response.json();
     return(
-        <div> 
-        <p> {data.titulo}</p>
-        <p>{data.preco}</p>
-        <p>{data.genero}</p>
-        <p>{data.tamanho}</p>
-
+        <>
+        <br/>
+        <div className={styles.container}> 
+        <p className={styles.titulo}>{data.titulo}</p>
+        <div className={styles.imagem}>
         <Image 
-              width= {150}
-              height={150}
+              width= {300}
+              height={300}
               src={data.imagem}/>
-
         </div>
+        <p className={styles.preco}>R${data.preco}</p>
+        <p className={styles.detalhes}>{data.genero}</p>
+        <p className={styles.detalhes}>{data.tamanho}</p>
+
         
+        </div>
+        </>
     );
 }
